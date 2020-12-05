5633ee8f98e7029f5fd72323b58614595b0563c0
eduardobcoelho <eduardobcoelho27@outlook.com>
2020-12-05 11:23:57 -0300

    questions updated

--- Q1.c
+++ Q1.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 #include <string.h>
-
+ 
 char *meuStrStr(const char *s1, const char *s2) {
-    int cont=0, tamanhoStringUm = strlen(s1), tamanhoStringDois = strlen(s2);
-    while (*s1 != '\0') {
-        if (*s1 == *s2) {
-            s1++; s2++;
-            cont++;
-            while (*s1 == *s2) {
-                s1++; s2++;
-                cont++;
-                if (cont == tamanhoStringDois) {
-                    return s1-cont;
-                }
-            }
-        } 
-        if(cont != 0) {
-          s1 -= cont;
-        }
-        s1++;
-        s2 -= (cont);
-        cont = 0;
-    }
-    return NULL;
+   int cont=0, tamanhoStringUm = strlen(s1), tamanhoStringDois = strlen(s2);
+   while (*s1 != '\0') {
+       if (*s1 == *s2) {
+           s1++; s2++;
+           cont++;
+           while (*s1 == *s2) {
+               s1++; s2++;
+               cont++;
+               if (cont == tamanhoStringDois) {
+                   return s1-cont;
+               }
+           }
+       }
+       if(cont != 0) {
+         s1 -= cont;
+       }
+       s1++;
+       s2 -= (cont);
+       cont = 0;
+   }
+   return NULL;
 }
-
+ 
 int main(int argc, char **argv) {
-    char primeraString[20], segundaString[20], *ponteiro;
-    scanf("%[^\n] %[^\n]", primeraString, segundaString);
-    ponteiro = meuStrStr(primeraString, segundaString);
-    printf("%p\n", ponteiro);
-    return 0;
+   char primeraString[20], segundaString[20], *endereco;
+   scanf("%[^\n] %[^\n]", primeraString, segundaString);
+   endereco = meuStrStr(primeraString, segundaString);
+   printf("%p\n", endereco);
+   return 0;
 }
--- Q3.c
+++ Q3.c
@@ -1,39 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+ 
 typedef struct ListaNumero {
-    int numero;
-    struct ListaNumero *proximo;
+   int numero;
+   struct ListaNumero *proximo;
 } NO;
+ 
 NO *inserirInicioLE(NO *inicio, int numero){
-    NO *novo;
-    novo = (NO*) malloc(sizeof(NO));
-    novo->numero = numero;
-    novo->proximo = inicio;
-    inicio = novo;
-    return inicio;
+   NO *novo;
+   novo = (NO*) malloc(sizeof(NO));
+   novo->numero = numero;
+   novo->proximo = inicio;
+   inicio = novo;
+   return inicio;
 }
+ 
 void mostrar(NO *atual){
-    while (atual != NULL) {
-        printf(" %d\t->\t", atual->numero);
-        atual = atual->proximo;
-    }
-    return;
+   while (atual != NULL) {
+       printf(" %d\t->\t", atual->numero);
+       atual = atual->proximo;
+   }
+   return;
 }
+ 
 void mostrarREC(NO *atual){
-    if(atual == NULL) return; 
-    printf(" %d\t->\t", atual->numero);
-    atual = atual->proximo;
-    mostrarREC(atual); 
+   if(atual == NULL) {
+       return;
+   }
+   printf(" %d\t->\t", atual->numero);
+   atual = atual->proximo;
+   mostrarREC(atual);
 }
+ 
 int main () {
-    int i;
-    NO *inicio=NULL; //ponteiro para inicio da lista
-    for(i=0;i<5;i++){
-        inicio=inserirInicioLE(inicio,i); //inserir nós NA Lista Encadeada
-    }
-    printf("Lista de todos os números cadastrados\n\n");
-    mostrar(inicio); //chama função NÃO recursiva para mostrar nós da Lista Encadeada
-    printf("\n");
-    printf("Lista de todos os números cadastrados\n\n");
-    mostrarREC(inicio); //chama função RECURSIVA que VOCÊ VAI DESENVOLVER que mostra os nós da Lista encadeada
+   int i;
+   NO *inicio=NULL; //ponteiro para inicio da lista
+   for(i=0;i<5;i++){
+       inicio=inserirInicioLE(inicio,i); //inserir nós NA Lista Encadeada
+   }
+   printf("Lista de todos os números cadastrados\n\n");
+   mostrar(inicio); //chama função NÃO recursiva para mostrar nós da Lista Encadeada
+   printf("\n");
+   printf("Lista de todos os números cadastrados\n\n");
+   mostrarREC(inicio); //chama função RECURSIVA que VOCÊ VAI DESENVOLVER que mostra os nós da Lista encadeada
 }
